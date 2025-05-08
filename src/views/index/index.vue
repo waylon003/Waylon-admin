74773f156a25eb08bafcb3f0a6e69d1a416a8297
North <1227379879@qq.com>
2025-05-09 00:41:18 +0800

    更新数据模型，添加tag字段并在表格中渲染相应标签

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,8 +1,9 @@
-<script setup lang="ts">
+<script setup lang="tsx">
 interface Item {
 	date: string
 	name: string
 	address: string
+	tag: string
 }
 
 const createData = (): Item[] => {
@@ -12,6 +13,7 @@ const createData = (): Item[] => {
 			date: '2016-05-03',
 			name: '王小明',
 			address: '上海市普陀区金沙江路 1518' + i,
+			tag: i % 2 === 0 ? 'primary' : 'success',
 		})
 	}
 	return arr
@@ -29,6 +31,26 @@ const column = ref([
 	{
 		prop: 'address',
 		label: '地址',
+	},
+	{
+		prop: 'tag',
+		label: 'tsx插槽标签',
+
+		/**
+		 *
+		 * @param {any} text 当前行的值
+		 * @param {Item} row  当前行数据
+		 * @param {number} index  行索引
+		 * @returns {JSX.Element} 返回一个JSX元素
+		 */
+		render: (text: any, row: Item, index: number) => {
+			return <el-tag type='primary'>{{ text }}</el-tag>
+		},
+	},
+	{
+		prop: 'tag',
+		label: 'slot标签',
+		slot: 'tag',
 	},
 ])
 const loading = ref(false)
@@ -60,7 +82,9 @@ const pageChange = (page: any, limit: any) => {
 		v-model:limit="limit"
 		:total="total"
 		@changePaging="pageChange"
-	></pro-table>
+	>
+		<template #tag="{ row, index }"> {{ row.tag }} -- {{ index }} </template>
+	</pro-table>
 </template>
 
 <style scoped></style>
